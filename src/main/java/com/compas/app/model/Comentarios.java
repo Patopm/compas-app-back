cf85722eabbe8673e996bba2f34b149c5f5700fa
thelastrebel14 <andreagaribaldi.dev@gmail.com>
2024-05-29 23:12:56 -0700

    Getters y setters para comentarios y publicaciones

--- src/main/java/com/compas/app/model/Comentarios.java
+++ src/main/java/com/compas/app/model/Comentarios.java
@@ -3,6 +3,7 @@ package com.compas.app.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "comentarios")
@@ -29,4 +30,97 @@ public class Comentarios {
     @JoinColumn(name = "publicacion_id", nullable = false)
     private Publicaciones publicacion;
 
+    public Long getId_comentario() {
+        return id_comentario;
+    }
+
+    public void setId_comentario(Long id_comentario) {
+        this.id_comentario = id_comentario;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public LocalDate getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDate created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDate getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDate updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Publicaciones getPublicacion() {
+        return publicacion;
+    }
+
+    public void setPublicacion(Publicaciones publicacion) {
+        this.publicacion = publicacion;
+    }
+
+    public Comentarios(){
+
+    }
+
+    public Comentarios(Long id_comentario, String comentario, LocalDate created_at, LocalDate updated_at, Usuario usuario, Publicaciones publicacion) {
+        this.id_comentario = id_comentario;
+        this.comentario = comentario;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.usuario = usuario;
+        this.publicacion = publicacion;
+    }
+
+    public Comentarios(String comentario, LocalDate created_at, LocalDate updated_at, Usuario usuario, Publicaciones publicacion) {
+        this.comentario = comentario;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.usuario = usuario;
+        this.publicacion = publicacion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comentarios that = (Comentarios) o;
+        return Objects.equals(id_comentario, that.id_comentario) && Objects.equals(comentario, that.comentario) && Objects.equals(created_at, that.created_at) && Objects.equals(updated_at, that.updated_at) && Objects.equals(usuario, that.usuario) && Objects.equals(publicacion, that.publicacion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_comentario, comentario, created_at, updated_at, usuario, publicacion);
+    }
+
+    @Override
+    public String toString() {
+        return "Comentarios{" +
+                "id_comentario=" + id_comentario +
+                ", comentario='" + comentario + '\'' +
+                ", created_at=" + created_at +
+                ", updated_at=" + updated_at +
+                ", usuario=" + usuario +
+                ", publicacion=" + publicacion +
+                '}';
+    }
 }
--- src/main/java/com/compas/app/model/Publicaciones.java
+++ src/main/java/com/compas/app/model/Publicaciones.java
@@ -2,6 +2,7 @@ package com.compas.app.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -35,4 +36,118 @@ public class Publicaciones {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
+
+    public Publicaciones(){}
+
+    public Publicaciones(Long idPublicacion, String titulo, String descripcion, byte[] multimedia, LocalDate createdAt, LocalDate updatedAt, List<Comentarios> comentarios, Usuario usuario) {
+        this.idPublicacion = idPublicacion;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.multimedia = multimedia;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.comentarios = comentarios;
+        this.usuario = usuario;
+    }
+
+    public Publicaciones(String titulo, String descripcion, byte[] multimedia, LocalDate createdAt, LocalDate updatedAt, List<Comentarios> comentarios, Usuario usuario) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.multimedia = multimedia;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.comentarios = comentarios;
+        this.usuario = usuario;
+    }
+
+    public Long getIdPublicacion() {
+        return idPublicacion;
+    }
+
+    public void setIdPublicacion(Long idPublicacion) {
+        this.idPublicacion = idPublicacion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public byte[] getMultimedia() {
+        return multimedia;
+    }
+
+    public void setMultimedia(byte[] multimedia) {
+        this.multimedia = multimedia;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<Comentarios> getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(List<Comentarios> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publicaciones that = (Publicaciones) o;
+        return Objects.equals(idPublicacion, that.idPublicacion) && Objects.equals(titulo, that.titulo) && Objects.equals(descripcion, that.descripcion) && Objects.deepEquals(multimedia, that.multimedia) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt) && Objects.equals(comentarios, that.comentarios) && Objects.equals(usuario, that.usuario);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idPublicacion, titulo, descripcion, Arrays.hashCode(multimedia), createdAt, updatedAt, comentarios, usuario);
+    }
+
+    @Override
+    public String toString() {
+        return "Publicaciones{" +
+                "idPublicacion=" + idPublicacion +
+                ", titulo='" + titulo + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", multimedia=" + Arrays.toString(multimedia) +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", comentarios=" + comentarios +
+                ", usuario=" + usuario +
+                '}';
+    }
 }
